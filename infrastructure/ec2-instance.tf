e4b18498c99178d9fea83e947a3986489d6eb813
Saravana Kumaar Dv <saravanakumaar.dv@hcltech.com>
2026-09-15 18:11:28 +0530

    M10 AWS compute concepts and POC completed

---
+++ infrastructure/ec2-instance.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "onboarding_ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name        = "onboarding-ec2"
+    Environment = "Learning"
+  }
+}
